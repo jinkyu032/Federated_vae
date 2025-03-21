668fec7256dcbbd62f4c98dc6246ac40d92c2aad
Lvvv11 <145366903+Lvvv11@users.noreply.github.com>
2025-03-21 17:54:02 +0900

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -3,11 +3,16 @@ from typing import Dict, Union
 from omegaconf import DictConfig
 
 def get_model(cfg: Union[Dict, DictConfig]):
-    latent_dim = cfg.model.latent_dim
-    conditional = cfg.model.conditional
-    num_classes = cfg.model.num_total_classes
+    if isinstance(cfg, DictConfig):
+        cfg_model = cfg.model
+    else:
+        cfg_model = cfg
+        
+    latent_dim = cfg_model.latent_dim
+    conditional = cfg_model.conditional
+    num_classes = cfg_model.num_total_classes
     sample_p = cfg.sample_p
-    if cfg.model.model_name == "vae":
+    if cfg_model.model_name == "vae":
         return VAE(latent_dim=latent_dim, conditional=conditional, num_classes=num_classes, sample_p=sample_p)
     else:
-        raise ValueError(f"Invalid model name: {cfg.model.model_name}")
+        raise ValueError(f"Invalid model name: {cfg_model.model_name}")
