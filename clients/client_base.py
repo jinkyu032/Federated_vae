e54badd0f8fe6acaa10996298da40fd50880d068
Lvvv11 <145366903+Lvvv11@users.noreply.github.com>
2025-03-21 14:52:20 +0900

    Update client_base.py

--- clients/client_base.py
+++ clients/client_base.py
@@ -3,6 +3,7 @@ from torch import optim
 import torch.nn as nn
 from torch.utils.data import DataLoader
 from typing import Dict, Optional
+from utils.logging_utils import AverageMeter
 __all__ = ['BaseClient']
 
 # Client class for federated learning
@@ -15,10 +16,14 @@ class BaseClient:
         self.optimizer = optim.Adam(self.model.parameters(), lr=cfg.lr)
         self.vae_loss = vae_loss
         self.vae_mu_target = vae_mu_target
-        self.alpha = cfg.alpha
 
     def train(self, local_epochs):
-        # self.model.load_state_dict(global_weights)
+
+        loss_meter = AverageMeter('Loss', ':.2f')
+        recon_loss_meter = AverageMeter('Recon Loss', ':.2f')
+        kl_loss_meter = AverageMeter('KL Loss', ':.2f')
+        dist_loss_meter = AverageMeter('Dist Loss', ':.2f')
+
         self.model.train()
         for _ in range(local_epochs):
             for data, target in self.data_loader:
@@ -26,10 +31,24 @@ class BaseClient:
                 target = target.to(self.device)
                 self.optimizer.zero_grad()
                 recon_batch, mu, log_var, z = self.model(data, target)
-                loss = sum(self.vae_loss(recon_batch, data, mu, log_var, mu_target=self.vae_mu_target, alpha=self.alpha, z=z))
+                recon_loss, kl_loss, dist_loss = self.vae_loss(recon_batch, data, mu, log_var, mu_target=self.vae_mu_target, alpha=self.alpha, z=z)
+                loss = recon_loss + kl_loss
                 loss.backward()
                 self.optimizer.step()
-        return self.model.state_dict()
+
+                loss_meter.update(loss.item(), data.size(0))
+                recon_loss_meter.update(recon_loss.item(), data.size(0))
+                kl_loss_meter.update(kl_loss.item(), data.size(0))
+                dist_loss_meter.update(dist_loss.item(), data.size(0))
+
+        
+        print(f"Training Loss: {loss_meter.avg:.2f}, Recon Loss: {recon_loss_meter.avg:.2f}, KL Loss: {kl_loss_meter.avg:.2f}")
+        loss_dict = {
+            "train_loss": loss_meter.avg,
+            "train_recon_loss": recon_loss_meter.avg,
+            "train_kl_loss": kl_loss_meter.avg
+        }
+        return self.model.state_dict(), loss_dict
 
     def update_model(self, global_weights):
         self.model.load_state_dict(global_weights)
